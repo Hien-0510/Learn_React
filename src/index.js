52de414c3d9bf9c2ea2846e6f848236b475782fc
Cupid <hien.lm1452@sinhvien.hoasen.edu.vn>
2023-09-18 21:17:11 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Person {
+  constructor(name, age, address) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+
+  description() {
+    return `${this.name} is ${this.age} years old and lives in ${this.address}`;
+  }
+}
+
+class People extends Person {
+  constructor(name,age,address,sex) {
+    super(name, age, address);
+    this.sex = sex;
+  }  
+  show() {
+      return `${this.description()} is ${this.sex}`;
+  }
+}
+
+const human = new People("Nguyen Van A", 20, "30 Nguyen Thai Hoc", "Male");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(human.show());
